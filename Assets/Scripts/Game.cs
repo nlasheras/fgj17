442ecde97f20981d557a32d264fbdbaf3b269569
Nacho Lasheras <fanatiko@gmail.com>
2017-01-21 22:45:23 +0200

    Added the call to StartGame to the Royal animator

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static Game Instance { get; private set; }
+
     public PeasantSpawner peasantSpawner;
 
-	void Start()
+
+    private void OnDestroy()
     {
+        Instance = null;
+    }
+
+    void Start()
+    {
+        Instance = this;
         peasantSpawner = GameObject.FindGameObjectWithTag("Respawn").GetComponent<PeasantSpawner>();
         peasantSpawner.enabled = false;
 	}
-	
+
+    public void StartGame()
+    {
+        peasantSpawner.enabled = true;
+        RoyalBehaviour.Instance.CarriageSpeed = 15;
+    }
+
 	void Update()
     {
 		
