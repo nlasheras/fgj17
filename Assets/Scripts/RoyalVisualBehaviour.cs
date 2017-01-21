adfb73c3f6653e81e4c559c8c1bc4e764fadc98d
Nacho Lasheras <fanatiko@gmail.com>
2017-01-21 17:31:49 +0200

    Royal prefab

---
+++ Assets/Scripts/RoyalVisualBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoyalVisualBehaviour : MonoBehaviour
+{
+    public Animator m_queen;
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+        m_queen.SetBool("Waving", InputSystem.Instance.IsWaving);	
+	}
+}
