eaff005a3bde6a4263a20ab6fbd0c99f405f1987
Jebuu <jepperi@gmail.com>
2017-01-21 18:59:18 +0200

    Added variations to peasants. Can tweak values for tickrate and amount of happiness generated. They spawn a placeholder textmesh when triggering happiness. Displayed cheer state is dependend on tickrate. Objectpooler now randomizes one of the peasants.

--- Assets/Scripts/ObjectPooler.cs
+++ Assets/Scripts/ObjectPooler.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour {
 
 
-    public GameObject GameObjectToPool;
-    public int PoolSize = 20;
+    public GameObject[] GameObjectsToPool;
+    // How many objects per pool
+    public int[] PoolSize;
     public bool CanGrow = true;
 
     private List<GameObject> m_pooledGameObjects;
@@ -21,47 +20,54 @@ public class ObjectPooler : MonoBehaviour {
     private void FillObjectPool()
     {
         // we create a container that will hold all the instances we create
-        m_waitingPool = new GameObject("[ObjectPooler] " + name);
+        m_waitingPool = new GameObject( "[ObjectPooler] " + name);
 
         m_pooledGameObjects = new List<GameObject>();
 
-        for (int i = 0; i < PoolSize; i++)
+        for (int j = 0; j < GameObjectsToPool.Length; j++)
         {
-            AddObjectToThePool();
+            for ( int i = 0; i < PoolSize[j]; i++)
+            {
+                AddObjectToThePool( GameObjectsToPool[j] );
+            }
         }
     }
 
     public GameObject GetPooledGameObject()
     {
+        int randomIndex = 0;
+
         for ( int i = 0; i < m_pooledGameObjects.Count; i++ )
         {
-            if (!m_pooledGameObjects[i].gameObject.activeInHierarchy)
-            {
-                return m_pooledGameObjects[ i ];
-            }
-        }
+            randomIndex = Random.Range( 0, m_pooledGameObjects.Count );
 
-        if ( CanGrow )
-        {
-            AddObjectToThePool();
+            if (!m_pooledGameObjects[ randomIndex ].gameObject.activeInHierarchy)
+            {
+                return m_pooledGameObjects[ randomIndex ];
+            }
+            else if (CanGrow)
+            {
+                return AddObjectToThePool(m_pooledGameObjects[randomIndex]);
+            }
         }
 
         return null;
     }
 
-    private void AddObjectToThePool()
+    private GameObject AddObjectToThePool(GameObject gameObjectToPool)
     {
-        if ( GameObjectToPool == null )
+        if (gameObjectToPool == null )
         {
             Debug.LogWarning("The " + gameObject.name + " No Object defined", gameObject);
-            return;
+            return null;
         }
 
-        GameObject newGameObject = Instantiate( GameObjectToPool );
+        GameObject newGameObject = Instantiate(gameObjectToPool);
 
         newGameObject.gameObject.SetActive( false );
         newGameObject.transform.parent = m_waitingPool.transform;
-        newGameObject.name = GameObjectToPool.name + "-" + m_pooledGameObjects.Count;
+        newGameObject.name = gameObjectToPool.name + "-" + m_pooledGameObjects.Count;
         m_pooledGameObjects.Add( newGameObject );
+        return newGameObject;
     }
 }
--- Assets/Scripts/Peasant.cs
+++ Assets/Scripts/Peasant.cs
@@ -6,23 +6,29 @@ using UnityEngine;
 public class Peasant : MonoBehaviour {
 
     // Movement
-    public float Speed = 0;
+    private float Speed = 15;
     public Vector2 Direction = Vector2.left;
 
     // Happiness
     public float HappinessTickRate = 1f;
     public float HappinessAmount = 1f;
+    public GameObject HappinessText;
+
+    // Sprites
+    public Sprite HappySprite;
+    public Sprite NeutralSprite;
 
     private float lastTick;
 
     private float m_initialSpeed;
     private Vector2 m_movement;
-
+    private SpriteRenderer m_spriteRenderer;
     
     // Use this for initialization
     void Awake()
     {
-        m_initialSpeed = Speed;    
+        m_initialSpeed = Speed;
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     protected virtual void OnEnable()
@@ -30,6 +36,12 @@ public class Peasant : MonoBehaviour {
         Speed = m_initialSpeed;
         // Peasants entering the view are not pleased right away ;(
         lastTick = Time.time;
+        SetToNeutral();
+    }
+
+    private void SetToNeutral()
+    {
+        m_spriteRenderer.sprite = NeutralSprite;
     }
 
     // Update is called once per frame
@@ -45,13 +57,25 @@ public class Peasant : MonoBehaviour {
     private void GenerateHappiness()
     {
         Debug.Log(gameObject.name+": I AM SO HAPPY!");
-        lastTick = Time.time;
+        m_spriteRenderer.sprite = HappySprite;
+
+        // Happiness amount added popup text
+        Vector3 textPosition = new Vector3( transform.position.x, transform.position.y + 1 );
+        Instantiate( HappinessText, textPosition, transform.rotation );
+
+        Invoke("SetToNeutral", HappinessTickRate * 0.5f);
+        lastTick = Time.time;       
     }
 
     public void Move()
     {
-        m_movement = Direction * (Speed / 10) * Time.deltaTime;
+        m_movement = Direction * ( Speed ) * Time.deltaTime;
         transform.Translate(m_movement, Space.World);
+
+        if (transform.position.x < 0)
+        {
+           m_spriteRenderer.flipX = true;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
