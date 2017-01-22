ebd1f6d689c596183d5018bfa443ab6e0b8ec143
Jeppe <jepperi@gmail.com>
2017-01-22 14:17:05 +0200

    Added dynamic effects script.

---
+++ Assets/DynamicEffects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class DynamicEffects : MonoBehaviour {
+
+    public float MinBloom;
+    public float MaxBloom;
+
+    private BloomOptimized bloomScript;
+
+    public float MinVignette;
+    public float MaxVignette;
+
+    private VignetteAndChromaticAberration vignetteScript;
+    // Use this for initialization
+    void Start () {
+        bloomScript = GetComponent<BloomOptimized>();
+        vignetteScript = GetComponent<VignetteAndChromaticAberration>();
+    }
+
+    // Update is called once per frame
+    void Update() { 
+
+        int joy = RoyalBehaviour.Instance.Joy;
+        bloomScript.threshold = MinBloom + (MaxBloom - MinBloom)*(joy / 100.0f);
+
+        int vignetteJoy = Mathf.Clamp(joy, 0, 20);
+        vignetteScript.intensity = MaxVignette + (MinVignette - MaxVignette)*(1 - joy/20.0f);
+	}
+}
